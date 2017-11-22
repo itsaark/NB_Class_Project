5b47c41aaff6b5938c30cfce8d7288a2a16973c4
Jordan Dearborn-Childs <jsd9@ada.cs.pdx.edu>
2017-11-21 16:01:58 -0800

    add initializeMap and header

---
+++ header.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "string_comapre.c"
+
+
+
+#define MAXSTRING 15 //The maximum number of character for a string
+#define LOCATIONS 12 //the numbers of cities on i26
+
+//Location Node
+typedef struct Location{
+	char name[MAXSTRING];
+	struct Location *east;
+	struct Location *west;
+	struct Location *north;
+	struct Location *south;
+}Location;
+
+//Function Prototypes
+
+int string_compare(char[], char[]);
+void inputCheck(char[], char[], Location[]);
+void initializeMap(Location[]);
+void printRoute (*char, *char, Location[]);
+
+
+
+
